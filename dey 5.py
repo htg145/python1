930492674150bb47fb6ce722739b67c74ec300ad
user <htg145@naver.com>
2025-02-25 21:32:32 +0900

    feat:day4

---
+++ dey 5.py
@@ -0,0 +1,69 @@
+#  대입 연산자
+# x = 10 #대입
+#  x += 10 # 더하고 대입(할당) x = x + 10
+#  x -= 5 #뺴고 대입 x = x - 5
+# x *= 3 # 곱하고 대입 x = x*3
+#  x /= 2 # 나누고(몫이 실수) x = x / 2 # 소수점
+# x //= 2 # 나누고(몫이 정수) 대입 (할당) x = x // 2 #정수
+# print(x)
+
+# 비교 연산자
+# x = 10
+# y = 20
+# z = 10
+# print(x == y) # 같다
+# print(x != z) # 같지 않다
+# print(x > y) # 왼쪽 기준 오른쪽 보다 크다
+# print(x < y) # 왼쪽 기준 오른쪽 보다 작다
+# print(x >= z) # 크거나 같다
+# print(x <= y) # 작거나 같다
+
+# 논리 연산자
+# a = True
+# b = False
+# print(a and  b)
+# print(a or (a and b))
+# print(not a)
+# print(not a and b)
+
+# 조건 연산자 (삼항 연산자)
+# if-else (아니면)
+# 조건식 이 참이 값1을 반환
+# 조건식 이 거짓 이면 값2을 반환
+
+# a = 10
+# b = 20
+# max_value = a if a > b else b
+# print(max_value)
+# if a > b:
+#     max_value = a
+# else:
+#     max_value = b
+# print(max_value)
+
+
+# score = 85
+# grade = "A" if score >= 90 else ("B" if score >= 80 else "C")
+# print(grade)
+#
+# if score >= 90:
+#     print("A")
+# elif score >= 80:
+#     print("B")
+# else:
+#     print("C")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
